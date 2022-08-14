ead195a47e43a708a7d4ea547b20114e27fd7c7f
DanielHealy <danielhealyhosting3@gmail.com>
2022-08-14 21:08:38 +0100

    added conditional render navbar

---
+++ frontend/src/components/NavbarLoggedIn.jsx
@@ -0,0 +1,47 @@
+import { ReactComponent as Flowerlogo } from "../Assets/flowerLogo.svg";
+import { ReactComponent as Logotextseperator } from "../Assets/logoSeperator.svg";
+import { ReactComponent as Hamburgermenu } from "../Assets/hamburgerMenu.svg";
+import { Link } from "react-router-dom";
+import "./navbarLoggedIn.css";
+import { useSelector } from "react-redux";
+import { FaAngleDown } from "react-icons/fa"
+
+
+export default function NavbarLoggedIn() {
+    const { user } = useSelector((state) => state.auth);
+  return (
+    <div className="navContainer">
+      <div className="logoContainerExt">
+        <Link to="/">
+          <div className="logoContainerInt">
+            <Flowerlogo className="logoImg" />
+            <p className="logoText1">PHLOX</p>
+            <Logotextseperator className="logoTextSeperator" />
+            <p className="logoText2">Agency</p>
+          </div>
+        </Link>
+      </div>
+      <div className="linkSectionExt">
+        <div className="linkSectionInt">
+          <p className="linkItemActive">Home</p>
+          <p className="linkItem">Portfolio</p>
+          <p className="linkItem">Blog</p>
+          <p className="linkItem">About</p>
+          <p className="linkItem">Contact</p>
+        </div>
+      </div>
+      <div className="buttonContainerExt">
+        <div className="buttonContainerInt">
+          <p className="user">
+            Welcome,{" "}
+           
+              {user.name}
+              <FaAngleDown />
+           
+          </p>
+        </div>
+      </div>
+      <Hamburgermenu className="hamburgerMenu" />
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import "./home.css";
 import Navbar from "../components/Navbar";
 import Splash from "../components/Splash";
+import NavbarLoggedIn from "../components/NavbarLoggedIn";
+import { useSelector} from "react-redux";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Splash />
-    </>
-  );
+    const { user } = useSelector((state) => state.auth);
+    if (user) {
+        return (
+            <>
+                <NavbarLoggedIn />
+                <Splash />
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <Navbar />
+                <Splash />
+            </>
+        )
+    }
 }
+
 
 export default App;
